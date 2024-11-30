c24ad1539149e092016614c5908361884152b498
Pedro <pedroph.nunes05@gmail.com>
2024-11-29 21:12:07 -0300

    sla

---
+++ java-variaveis/src/operadores.java
@@ -0,0 +1,14 @@
+public class operadores {
+
+    public static void main(String [] argrs){
+
+        int numero = 5;
+
+        numero = - numero;
+
+        System.out.println(numero);
+    }
+
+
+
+}
