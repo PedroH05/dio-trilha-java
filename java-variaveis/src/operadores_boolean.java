80d829e9d51e2a4a9034a9a53b3ba658edfe52e2
Pedro <pedroph.nunes05@gmail.com>
2024-11-29 21:43:24 -0300

    boolean

---
+++ java-variaveis/src/operadores_boolean.java
@@ -0,0 +1,12 @@
+public class operadores_boolean {
+    public static void main(String[] args) {
+        boolean variavel = true;
+        System.out.println(!variavel);
+
+        variavel = !variavel;
+        System.out.println(variavel);
+        
+        //isso serve para transformar ela em false
+
+    }
+}
