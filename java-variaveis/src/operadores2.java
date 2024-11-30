f566e77554aa5d53f76810b339652ffc8384ff27
Pedro <pedroph.nunes05@gmail.com>
2024-11-29 21:37:42 -0300

    atribuicao op.

---
+++ java-variaveis/src/operadores2.java
@@ -0,0 +1,18 @@
+public class operadores2 {
+    public static void main(String[] args) {
+        
+    int numero = 5; 
+    //atribuicao de valor.
+    numero = numero + 2;
+    //numero++ = adiciona +1 no final.
+    System.out.println(numero);
+
+    //if numero = numero++ 
+   //precisa de mais um system.out...
+   //numero = 6; 
+
+    //numero--
+    //numero = 4;
+    
+    }
+} 
