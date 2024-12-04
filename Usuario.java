2e8a72d4f19216d8456944fcbcac8c5dd8a0bcee
PedroH05 <pedroph.nunes05@gmail.com>
2024-12-04 18:32:18 -0300

    Add files via upload

---
+++ Usuario.java
@@ -0,0 +1,13 @@
+public class Usuario {
+    
+    public static void main(String[] args) throws Exception {
+
+    SmartTv smartTv = new SmartTv();
+
+    System.out.println("Tv ligada? " + smartTv.ligada);
+    System.out.println("Qual canal esta a tv?" + smartTv.canal);
+    System.out.println("Qual o volume da tv?" + smartTv.volume);
+
+
+    }
+}
