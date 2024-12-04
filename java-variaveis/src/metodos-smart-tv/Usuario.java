f5b4d6a2de1e192ff9de3b4dd283759c17bd1ea4
Pedro <pedroph.nunes05@gmail.com>
2024-12-04 18:54:55 -0300

    smart tv

---
+++ java-variaveis/src/metodos-smart-tv/Usuario.java
@@ -0,0 +1,12 @@
+public class Usuario {
+    
+    public static void main(String[] args) throws Exception {
+
+    SmartTv smartTv = new SmartTv();
+
+    System.out.println("Tv ligada? " + smartTv.ligada);
+    System.out.println("canal tv: " + smartTv.canal);
+    System.out.println("volume da tv: " + smartTv.volume);
+    
+    }
+}
