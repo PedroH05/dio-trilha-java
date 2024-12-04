f97a5b3630446bd2a5ec8491fdb962af234cd271
Pedro <pedroph.nunes05@gmail.com>
2024-12-04 18:56:19 -0300

    smartv

--- java-variaveis/src/metodos-smart-tv/Usuario.java
+++ java-variaveis/src/metodos-smart-tv/Usuario.java
@@ -7,6 +7,9 @@ public class Usuario {
     System.out.println("Tv ligada? " + smartTv.ligada);
     System.out.println("canal tv: " + smartTv.canal);
     System.out.println("volume da tv: " + smartTv.volume);
-    
+        
+    smartTv.ligar ();
+    System.out.println(" novo status -> Tv ligada? " + smartTv.ligada);
+
     }
 }
