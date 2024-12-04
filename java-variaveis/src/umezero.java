70d4f7c813e36fe6fa85420025722b8fe516f3d0
Pedro <pedroph.nunes05@gmail.com>
2024-12-04 15:43:46 -0300

    1 e 0

---
+++ java-variaveis/src/umezero.java
@@ -0,0 +1,19 @@
+public class umezero {
+
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 5;
+        b = 5;  
+
+        String resultado = "";
+        
+        if(a==b)
+            resultado = "verdadeiro";
+        else 
+            resultado = "falso";
+                
+        System.out.println(resultado);
+    }
+}
+
