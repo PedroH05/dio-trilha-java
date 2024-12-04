7ffdfb7391d3077864b5edca809657f9e9740685
Pedro <pedroph.nunes05@gmail.com>
2024-12-04 16:10:19 -0300

    objetos

---
+++ java-variaveis/src/objetos.java
@@ -0,0 +1,34 @@
+public class objetos {
+    
+    public static void main(String[] args) {
+
+            int numeroUm = 1;
+            int numeroDois = 2;
+
+            boolean simNao = numeroUm == numeroDois;
+            System.out.println("NumeroUm e igual a numeroDois?" );
+            System.out.println(simNao);
+                
+            simNao = numeroUm != numeroDois;
+            System.out.println("NumeroUm e diferente de numeroDois?");
+            System.out.println(simNao);
+
+            simNao = numeroUm > numeroDois;
+            System.out.println("numeroUm e maior que numeroDois?");
+            System.out.println(simNao);
+
+            //objetos
+
+            String nomeUm = "Pedro";
+            String nomeDois = "Pedro";
+
+            System.out.println(nomeUm == nomeDois);
+
+            String nomeTres = "Henrique";
+            String nomeQuattro= new String("Carvalho");
+
+            System.out.println(nomeTres.equals(nomeQuattro));
+            //serve para conseguir comparar duas string. Objetos nao sao guardados na memoria igual um numero    
+    }
+
+}
