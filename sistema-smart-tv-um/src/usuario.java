8c3c675391142c2ba03e37f6979a435390cad1cb
Pedro <pedroph.nunes05@gmail.com>
2024-12-04 19:21:27 -0300

    smart tv

---
+++ sistema-smart-tv-um/src/usuario.java
@@ -0,0 +1,11 @@
+public class usuario {
+
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv ();
+
+        System.out.println(smartTv.ligada);
+        System.out.println(smartTv.canal); 
+        System.out.println(smartTv.volume);
+    }
+}
